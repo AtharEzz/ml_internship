0ba84015c9826e0bd47979daf7730a55f3017615
Athar <Athar.Ezzeldin@gmail.com>
2025-10-10 17:50:23 +0300

    Updating the app.py

--- 4_movie_recommendation/notebooks/app.py
+++ 4_movie_recommendation/notebooks/app.py
@@ -45,48 +45,7 @@ svd = load_model()
 user_similarity_df, train_ratings, movies = load_user_based_components()
 
 # train_ratings, movies = load_data()
-
-# # Get list of users
-# user_ids = sorted(train_ratings['user_id'].unique())
-
-# # App title
-# st.title("Movie Recommender System")
-# st.markdown("Built with **SVD Matrix Factorization**")
-
-# # User input
-# st.sidebar.header("Settings")
-# user_id = st.sidebar.selectbox("Select User ID", user_ids)
-# n_recs = st.sidebar.slider("Number of Recommendations", 1, 20, 5)
-
-# # Function to get recommendations
-# def get_recommendations(user_id, n_recs):
-    # # Get movies user has rated
-    # rated = set(train_ratings[train_ratings['user_id'] == user_id]['movie_id'])
-    # all_movies = set(movies['movie_id'])
-    # candidates = list(all_movies - rated)
-    
-    # # Predict ratings
-    # predictions = []
-    # for mid in candidates:
-        # pred = svd.predict(uid=user_id, iid=mid)
-        # predictions.append((mid, pred.est))
-    
-    # # Sort and return top N
-    # predictions.sort(key=lambda x: x[1], reverse=True)
-    # return predictions[:n_recs]
-
-# # Generate recommendations
-# if st.sidebar.button("Get Recommendations"):
-    # with st.spinner("Generating recommendations..."):
-        # recs = get_recommendations(user_id, n_recs)
-    
-    # st.subheader(f"Top {n_recs} Recommendations for User {user_id}")
-    
-    # for i, (mid, score) in enumerate(recs, 1):
-        # title = movies[movies['movie_id'] == mid]['title'].iloc[0]
-        # st.write(f"{i}. **{title}** (Predicted Rating: {score:.2f})")
-# else:
-    # st.info("Select a user and click 'Get Recommendations' to start!")
+  
     
     
 # Get list of users (from train_ratings)
